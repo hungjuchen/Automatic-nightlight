e4bf8042c9afecef828dc8d6e696d3e41c8cf0a6
Hung-Ju Chen <hongru1015@gmail.com>
2019-04-09 01:25:30 -0400

    Update codehw2.py

--- codehw2.py
+++ codehw2.py
@@ -8,25 +8,25 @@ while True:
     print((reading,))   # Open "Plotter" to get values from the light sensor while running
 
     if reading >= 386:  # reading higher values = environment less light
-        boat = Image("99999:99999:99999:99999:99999")
-        display.show(boat)
+        led = Image("99999:99999:99999:99999:99999")    # Define "led" as output light
+        display.show(led)
 
     elif reading >= 365:
-        boat = Image("00000:99999:99999:99999:99999")
-        display.show(boat)
+        led = Image("00000:99999:99999:99999:99999")   # 0 to 9 represent the brightness levels of each LED
+        display.show(led)
 
     elif reading >= 350:
-        boat = Image("00000:00000:99999:99999:99999")
-        display.show(boat)
+        led = Image("00000:00000:99999:99999:99999")
+        display.show(led)
 
     elif reading >= 330:
-        boat = Image("00000:00000:00000:99999:99999")
-        display.show(boat)
+        led = Image("00000:00000:00000:99999:99999")
+        display.show(led)
 
     elif reading >= 290:
-        boat = Image("00000:00000:00000:00000:99999")
-        display.show(boat)
+        led = Image("00000:00000:00000:00000:99999")
+        display.show(led)
 
     else:
-        boat = Image("03030:33333:33333:03330:00300")   #DIY dimmer Image.HEART
-        display.show(boat)
+        led = Image("03030:33333:33333:03330:00300")   #DIY dimmer Image.HEART
+        display.show(led)
